444c33626877c724c9a06db8dd8c7f7b8e95cd97
Priyanka <nikki.chowdhury@gmail.com>
2025-09-18 15:55:42 -0400

    Implementation of Bigram Prob & Spacy Embeddings

--- app/main.py
+++ app/main.py
@@ -12,7 +12,10 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.bigram_model import BigramModel
+import spacy
+
 app = FastAPI()
+
 # Sample corpus for the bigram model
 corpus = [
 "The Count of Monte Cristo is a novel written by Alexandre Dumas. \
@@ -22,14 +25,27 @@ It tells the story of Edmond Dantès, who is falsely imprisoned and later seeks 
 "bigram models are simple but effective"
 ]
 bigram_model = BigramModel(corpus)
+nlp = spacy.load("en_core_web_lg")
+
 class TextGenerationRequest(BaseModel):
     start_word: str
     length: int
+
+class EmbeddingRequest(BaseModel):
+    word: str
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 @app.post("/generate")
 def generate_text(request: TextGenerationRequest):
     generated_text = bigram_model.generate_text(request.start_word, request.length)
-    
     return {"generated_text": generated_text}
+
+@app.post("/embeddings")
+def embeddings(request: EmbeddingRequest):
+    print("LOOK FOR REQUEST WORD", request.word)
+    print(nlp)
+    emb_word = nlp(request.word)
+    print("LOOK FOR WORD", emb_word)
+    return emb_word.vector
